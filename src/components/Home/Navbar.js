5846618360db72994cd18f56379fc029e80b0992
FrugalShubham <77122104+FrugalShubham@users.noreply.github.com>
2024-05-06 02:38:47 +0530

    ChangeRespoForSomeCodeIssue

---
+++ src/components/Home/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {toTitle } from "../../utilities/tools";
+import SelectionFilter from "../common/filterBar";
+import { setFilter } from "../../features/filterSlice";
+
+const Navbar = () => {
+  const { filterSlice } = useSelector((data) => data);
+  const dispatch = useDispatch();
+  const applyFilter = (key, newval) => {
+    dispatch(setFilter({ key, value: newval }));
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "start",
+        flexWrap: "wrap",
+      }}
+    >
+      {Object.entries(filterSlice).map(([key, val]) => (
+        <div style={{ minWidth: "190px" }} key={key}>
+          <SelectionFilter
+            label={toTitle(key)}
+            placeholder={toTitle(key)}
+            options={val.options}
+            value={val.value}
+            setValue={(newval) => applyFilter(key, newval)}
+            size="small"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Navbar;
